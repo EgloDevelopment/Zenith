7a7b943e6d0eed05c6e4bbbf30447fafabcf5cad
EgloDevelopment <UNUstifA4W346TcF2Qwc@gmail.com>
2024-04-24 18:14:58 -0600

    Fixed CORS lol

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
-const magic = require('express-routemagic')
+const magic = require("express-routemagic");
 const port = 3000;
 
 app.use(express.json());
-magic.use(app)
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+magic.use(app);
 
 app.listen(port, () => {
     console.log(`Zenith server listening on port ${port}`);
